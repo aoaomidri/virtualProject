e3a93afb86236bc77ea7662cf9a03d39e9019010
aoaomidri <aoaomidori2723@gmail.com>
2024-01-14 21:48:30 +0900

    externalの中にimguiを移動

--- Engine/Base/WinApp.h
+++ Engine/Base/WinApp.h
@@ -6,9 +6,9 @@
 #include<dxgidebug.h>
 #pragma comment(lib,"dxguid.lib")
 
-#include<imgui.h>
-#include<imgui_impl_dx12.h>
-#include<imgui_impl_win32.h>
+#include"../../externals/Imgui/imgui.h"
+#include"../../externals/Imgui/imgui_impl_dx12.h"
+#include"../../externals/Imgui/imgui_impl_win32.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
